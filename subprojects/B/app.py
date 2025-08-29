17c243dfafff28508bd03024a70396ccde4ef31a
Ben Colquhoun <benjamincahoonz@gmail.com>
2025-08-29 18:03:13 +1200

    feat: Initial commit

---
+++ subprojects/B/app.py
@@ -0,0 +1,20 @@
+import sys
+
+from PySide6.QtWidgets import QMainWindow, QApplication
+
+from ui.library_widget import LibraryWidget
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Test Library")
+
+        self.library = LibraryWidget()
+        self.setCentralWidget(self.library)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec()
