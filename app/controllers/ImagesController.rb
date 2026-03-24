8d93e1e4121b872730879a22dd8723ed05455bc3
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-03-24 18:26:06 +1300

    feat: Add ImageController

---
+++ app/controllers/ImagesController.rb
@@ -0,0 +1,53 @@
+class ImagesController < ApplicationController
+  before_action :set_image, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_album, only: [ :new, :create ]
+
+  def show
+  end
+
+  def new
+    @image = @album.images.build(campaign: @album.campaign)
+  end
+
+  def create
+    @image = @album.images.build(image_params)
+    @image.campaign = @album.campaign
+
+    if @image.save
+      redirect_to @album, notice: "Image uploaded successfully"
+    else
+      redirect_to :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @image.update(image_params)
+      redirect_to @image, notice: "Image updated successfully"
+    else
+      redirect_to :edit, notice: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    album = @image.album
+    @image.destroy
+    redirect_to album, notice: "Image deleted successfully"
+  end
+
+  private
+
+  def set_image
+    @image = Image.find(params[:id])
+  end
+
+  def set_album
+    @album = Album.find(params[:album_id])
+  end
+
+  def image_params
+    params.expect(image: [ :title, :notes, :file ])
+  end
+end
