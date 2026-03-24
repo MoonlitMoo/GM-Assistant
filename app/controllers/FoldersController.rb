9b963f3cbc42eade055293f5dce959e11e459d8e
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-03-24 17:01:46 +1300

    feat: Adding FolderController + adjust routes
    Routes are no longer deeply nested. Folder creation is done only from
    the parent folder, since the campaign root folder is pre-generated.

---
+++ app/controllers/FoldersController.rb
@@ -0,0 +1,65 @@
+class FoldersController < ApplicationController
+    before_action :set_folder, only: [ :show, :edit, :update, :destroy ]
+    before_action :set_campaign, only: [ :index ]
+    before_action :set_parent_from_folder, only: [ :new, :create ], if: -> { params[:folder_id].present? }
+
+    def index
+      @folders = @campaign.folders.order(:name)
+    end
+
+    def show
+      @child_folders = @folder.child_folders.order(:name)
+      @albums = @folder.albums.order(:name)
+    end
+
+    def new
+      # Create the folder using the parent found via the before_actions
+      @folder = @parent.child_folders.build(campaign: @parent.campaign)
+    end
+
+    def create
+      @folder = @parent.child_folders.build(folder_params)
+      @folder.campaign = @parent.campaign
+
+      if @folder.save
+        redirect_to @parent, notice: "Folder created successfully"
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    def edit
+    end
+
+    def update
+      if @folder.update(folder_params)
+        redirect_to @folder, notice: "Folder updated successfully"
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+
+    def destroy
+      parent = @folder.parent || @folder.campaign
+      @folder.destroy
+      redirect_to parent, notice: "Folder destroyed successfully"
+    end
+
+    private
+
+    def set_folder
+      @folder = Folder.find(params[:id])
+    end
+
+    def set_campaign
+      @campaign = Campaign.find(params[:id])
+    end
+
+    def set_parent_from_folder
+      @parent = Folder.find(params[:folder_id])
+    end
+
+    def folder_params
+      params.expect(folder: [ :name, :description ])
+    end
+end
