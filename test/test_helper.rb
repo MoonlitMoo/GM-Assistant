25578b92cb2e828807d03d122e57c6988ad2ed95
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-03-28 10:22:15 +1300

    test: Added coverage with SimpleCov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,12 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
+require "simplecov"
 require "rails/test_help"
+
+SimpleCov.start "rails" do
+  add_filter "/app/mailers/application_mailer.rb"
+  add_filter "/app/jobs/application_job.rb"
+end
 
 module ActiveSupport
   class TestCase
