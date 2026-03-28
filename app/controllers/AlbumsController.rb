67be3695d1322836fbc4f18ff5ad6e689f0f1f0b
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-03-28 13:26:22 +1300

    feat: Added basic library tree for Folders and Albums
    Implements a basic javascript based browsable tree. The folders and
    albums are sorted by name and separated at this most basic level.
    
    A default top bar is added without formatting.

--- app/controllers/AlbumsController.rb
+++ app/controllers/AlbumsController.rb
@@ -1,53 +1,59 @@
 class AlbumsController < ApplicationController
-    before_action :set_album, only: [ :show, :edit, :update, :destroy ]
-    before_action :set_folder, only: [ :new, :create ], if: -> { params[:folder_id].present? }
+  layout "campaign"
+  before_action :set_album, only: [ :show, :edit, :update, :destroy ]
+  before_action :set_folder, only: [ :new, :create ], if: -> { params[:folder_id].present? }
+  before_action :set_campaign, only: [ :show ]
 
-    def show
-      @images = @album.images.with_attached_file.order(created_at: :desc)
+  def show
+    @images = @album.images.with_attached_file.order(created_at: :desc)
+  end
+
+  def new
+    @album = @folder.albums.build(campaign: @folder.campaign)
+  end
+
+  def create
+    @album = @folder.albums.build(album_params)
+    @album.campaign = @folder.campaign
+    if @album.save
+      redirect_to @album, notice: "Album created successfully"
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
 
-    def new
-      @album = @folder.albums.build(campaign: @folder.campaign)
+  def edit
+  end
+
+  def update
+    if @album.update(album_params)
+      redirect_to @album, notice: "Album updated successfully"
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
 
-    def create
-      @album = @folder.albums.build(album_params)
-      @album.campaign = @folder.campaign
-      if @album.save
-        redirect_to @album, notice: "Album created successfully"
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
+  def destroy
+    parent = @album.folder
+    @album.destroy
+    redirect_to parent, notice: "Album destroyed successfully"
+  end
 
-    def edit
-    end
+  private
 
-    def update
-      if @album.update(album_params)
-        redirect_to @album, notice: "Album updated successfully"
-      else
-        render :edit, status: :unprocessable_entity
-      end
-    end
+  def set_album
+    @album = Album.find(params[:id])
+  end
 
-    def destroy
-      parent = @album.folder
-      @album.destroy
-      redirect_to parent, notice: "Album destroyed successfully"
-    end
+  def set_folder
+    @folder = Folder.find(params[:folder_id])
+  end
 
-    private
+  def set_campaign
+    @campaign = @album.campaign
+  end
 
-    def set_album
-      @album = Album.find(params[:id])
-    end
-
-    def set_folder
-      @folder = Folder.find(params[:folder_id])
-    end
-
-    def album_params
-      params.expect(album: [ :name, :description ])
-    end
+  def album_params
+    params.expect(album: [ :name, :description ])
+  end
 end
