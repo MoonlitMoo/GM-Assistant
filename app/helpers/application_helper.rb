ebd33af1f85b10db1d5637d72a5901133d7bde9d
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-03-28 14:27:33 +1300

    feat: Application layout with sidebar tree

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def content_body_frame(&block)
+    content = capture(&block)
+    return content unless turbo_frame_request?
+
+    turbo_frame_tag("content-body", **content_body_frame_options) { content }
+  end
+
+  def content_body_frame_options
+    {
+      data: {
+        turbo_action: "advance"
+      }
+    }
+  end
 end
