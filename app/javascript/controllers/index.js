db09015caedc6e0c3fc89df5852abd93bed2acaf
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-03-28 17:04:06 +1300

    feat: Refactor JS to use Vite instead of importmap
    Prepping for React integration.

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,10 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "./application"
+import CampaignTreeController from "./campaign_tree_controller"
+import ContentPanelController from "./content_panel_controller"
+import HelloController from "./hello_controller"
+import TreeRefreshController from "./tree_refresh_controller"
+
+application.register("campaign-tree", CampaignTreeController)
+application.register("content-panel", ContentPanelController)
+application.register("hello", HelloController)
+application.register("tree-refresh", TreeRefreshController)
