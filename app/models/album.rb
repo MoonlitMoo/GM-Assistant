e33db6ddb90b4c6131f9d3d69394c6b874be1d67
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-03-28 15:21:35 +1300

    feat: Album show page has image thumbnails and better UI
    A more polished, though not final UI for the album show page.
    The dev and tests use imagemagick for thumbnail generation.

--- app/models/album.rb
+++ app/models/album.rb
@@ -2,7 +2,7 @@ class Album < ApplicationRecord
   belongs_to :campaign
   belongs_to :folder
 
-  has_many :images, dependent: :destroy
+  has_many :images, -> { order(position: :asc) }, dependent: :destroy
 
   validates :name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_23_231549) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_28_001000) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_23_231549) do
     t.integer "campaign_id", null: false
     t.datetime "created_at", null: false
     t.text "notes"
+    t.integer "position"
     t.string "title"
     t.datetime "updated_at", null: false
     t.index ["album_id"], name: "index_images_on_album_id"
