c71158d3ff142e7322a5dc45e7c87cc3b1cfadd5
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-04-01 19:13:29 +1300

    feat: Added recently presented area to GM controller

--- test/requests/player_displays_controller_test.rb
+++ test/requests/player_displays_controller_test.rb
@@ -11,7 +11,7 @@ class PlayerDisplaysControllerTest < ActionDispatch::IntegrationTest
       "image_id" => image.id
     }
 
-    assert_difference("PresentationEvent.count", 1) do
+    assert_no_difference("PresentationEvent.count") do
       assert_no_difference("PlayerDisplay.count") do
         assert_broadcast_on("player_display_#{image.campaign_id}", expected_payload) do
           patch present_campaign_player_display_path(image.campaign),
@@ -24,12 +24,26 @@ class PlayerDisplaysControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_equal expected_payload, JSON.parse(response.body)
     assert_equal image.id, player_display.reload.current_image_id
+  end
+
+  test "present changing the image creates a presentation event for the previously shown image" do
+    previous_image = create(:image)
+    next_image = create(:image, campaign: previous_image.campaign)
+    create(:player_display, campaign: previous_image.campaign, current_image: previous_image)
+
+    assert_difference("PresentationEvent.count", 1) do
+      patch present_campaign_player_display_path(previous_image.campaign),
+            params: { current_image_id: next_image.id },
+            as: :json
+    end
+
+    assert_response :success
 
     presentation_event = PresentationEvent.order(:created_at).last
-    assert_equal image.campaign, presentation_event.campaign
-    assert_equal image, presentation_event.image
+    assert_equal previous_image.campaign, presentation_event.campaign
+    assert_equal previous_image, presentation_event.image
     assert_equal "presented", presentation_event.event_type
-    assert_equal image.title, presentation_event.image_title
+    assert_equal previous_image.title, presentation_event.image_title
   end
 
   test "present lazily creates the player display if one does not exist" do
@@ -37,7 +51,7 @@ class PlayerDisplaysControllerTest < ActionDispatch::IntegrationTest
 
     assert_nil image.campaign.player_display
 
-    assert_difference("PresentationEvent.count", 1) do
+    assert_no_difference("PresentationEvent.count") do
       assert_difference("PlayerDisplay.count", 1) do
         patch present_campaign_player_display_path(image.campaign),
               params: { current_image_id: image.id },
@@ -50,6 +64,27 @@ class PlayerDisplaysControllerTest < ActionDispatch::IntegrationTest
     player_display = image.campaign.reload.player_display
     assert_not_nil player_display
     assert_equal image.id, player_display.current_image_id
+  end
+
+  test "presenting the same image is ignored and does not create a presentation event" do
+    player_display = create(:player_display, :with_current_image)
+    image = player_display.current_image
+    expected_payload = {
+      "image_url" => rails_blob_url(image.file, host: "www.example.com"),
+      "image_id" => image.id
+    }
+
+    assert_no_difference("PresentationEvent.count") do
+      assert_broadcasts("player_display_#{player_display.campaign_id}", 0) do
+        patch present_campaign_player_display_path(player_display.campaign),
+              params: { current_image_id: image.id },
+              as: :json
+      end
+    end
+
+    assert_response :success
+    assert_equal expected_payload, JSON.parse(response.body)
+    assert_equal image.id, player_display.reload.current_image_id
   end
 
   test "present turbo stream updates the topbar status" do
@@ -86,8 +121,9 @@ class PlayerDisplaysControllerTest < ActionDispatch::IntegrationTest
 
   test "clear clears the current image, broadcasts the change, and returns json" do
     player_display = create(:player_display, :with_current_image)
+    previous_image = player_display.current_image
 
-    assert_difference("PresentationEvent.count", 1) do
+    assert_difference("PresentationEvent.count", 2) do
       assert_broadcast_on("player_display_#{player_display.campaign_id}", { "cleared" => true }) do
         patch clear_campaign_player_display_path(player_display.campaign), as: :json
       end
@@ -97,7 +133,28 @@ class PlayerDisplaysControllerTest < ActionDispatch::IntegrationTest
     assert_equal({ "cleared" => true }, JSON.parse(response.body))
     assert_nil player_display.reload.current_image
 
-    presentation_event = PresentationEvent.order(:created_at).last
+    presented_event = PresentationEvent.presented.order(:created_at).last
+    cleared_event = PresentationEvent.cleared.order(:created_at).last
+
+    assert_equal player_display.campaign, presented_event.campaign
+    assert_equal previous_image, presented_event.image
+    assert_equal previous_image.title, presented_event.image_title
+
+    assert_equal player_display.campaign, cleared_event.campaign
+    assert_nil cleared_event.image
+    assert_nil cleared_event.image_title
+  end
+
+  test "clear creates a presentation event with event type cleared" do
+    player_display = create(:player_display, :with_current_image)
+
+    assert_difference("PresentationEvent.count", 2) do
+      patch clear_campaign_player_display_path(player_display.campaign), as: :json
+    end
+
+    assert_response :success
+
+    presentation_event = PresentationEvent.cleared.order(:created_at).last
     assert_equal player_display.campaign, presentation_event.campaign
     assert_equal "cleared", presentation_event.event_type
     assert_nil presentation_event.image
