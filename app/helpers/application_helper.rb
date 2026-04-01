c2d687e7d3791587d8a3d3b216913ddf6fce11c4
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-04-01 19:51:30 +1300

    fix: Exiting a workflow returns correctly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -25,4 +25,35 @@ module ApplicationHelper
   def tree_refresh_marker
     tag.div(nil, hidden: true, data: { controller: "tree-refresh" })
   end
+
+  def form_navigation_params
+    current_path = safe_return_to_path(request.fullpath)
+    current_path.present? ? { return_to: current_path } : {}
+  end
+
+  def form_cancel_path(default_path)
+    form_return_to_value || default_path
+  end
+
+  def form_return_to_field
+    return unless form_return_to_value.present?
+
+    hidden_field_tag :return_to, form_return_to_value
+  end
+
+  private
+
+  def form_return_to_value
+    safe_return_to_path(params[:return_to])
+  end
+
+  def safe_return_to_path(path)
+    value = path.to_s
+    return if value.blank?
+    return if value.start_with?("//")
+    return unless value.start_with?("/")
+    return if value.match?(/[\r\n]/)
+
+    value
+  end
 end
