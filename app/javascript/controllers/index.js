0f4e92ddf272857a0a539206cf867650b25a6553
Benjamin Colquhoun <benjamincahoonz@gmail.com>
2026-04-04 20:56:18 +0900

    chore: clean up scaffolding files

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,12 +1,10 @@
 import { application } from "./application"
 import AutoSubmitController from "./auto_submit_controller"
 import ContentPanelController from "./content_panel_controller"
-import HelloController from "./hello_controller"
 import PlayerDisplayController from "./player_display_controller"
 import TreeRefreshController from "./tree_refresh_controller"
 
 application.register("auto-submit", AutoSubmitController)
 application.register("content-panel", ContentPanelController)
-application.register("hello", HelloController)
 application.register("player-display", PlayerDisplayController)
 application.register("tree-refresh", TreeRefreshController)
