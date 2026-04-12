01b1a4c9aef39bf801b4afd456e156d781fbc194
Ben Colquhoun <benjamincahoonz@gmail.com>
2026-04-12 17:13:33 +0900

    fix: Issues tests raised

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def after_sign_out_path_for(_resource_or_scope)
+    new_user_session_path
+  end
+
   def set_current_user
     Current.user = current_user
   end
