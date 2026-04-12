67fe4d0e450065b48215b9a323945560f103deb1
Ben Colquhoun <benjamincahoonz@gmail.com>
2026-04-12 17:13:23 +0900

    test: Update tests for devise

--- test/requests/passwords_controller_test.rb
+++ test/requests/passwords_controller_test.rb
@@ -5,59 +5,65 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
     @user = User.take || create(:user)
   end
 
-  test "get new while signed in redirects to current user's password form" do
+  test "get new while signed in redirects to the signed-in landing page" do
     sign_in(@user)
 
-    get new_password_path
+    get new_user_password_path
 
     assert_response :redirect
-
-    token = response.location[%r{/passwords/(.+)/edit}, 1]
-
-    assert_not_nil token
-    assert_equal @user, User.find_by_password_reset_token!(token)
+    assert_redirected_to root_path
   end
 
-  test "post create with valid email redirects and enqueues mail" do
-    assert_enqueued_email_with PasswordsMailer, :reset, args: [ @user ] do
-      post passwords_path, params: { email_address: @user.email_address }
+  test "post create with valid email redirects and sends mail" do
+    assert_emails 1 do
+      post user_password_path, params: { user: { email_address: @user.email_address } }
     end
 
-    assert_redirected_to new_session_path
+    assert_redirected_to new_user_session_path
   end
 
   test "post create with unknown email redirects without leaking account presence" do
     assert_enqueued_emails 0 do
-      post passwords_path, params: { email_address: "missing-user@example.com" }
+      post user_password_path, params: { user: { email_address: "missing-user@example.com" } }
     end
 
-    assert_redirected_to new_session_path
+    assert_redirected_to new_user_session_path
   end
 
   test "get edit with valid token returns success" do
-    get edit_password_path(@user.password_reset_token)
+    token = @user.send_reset_password_instructions
+
+    get edit_user_password_path(reset_password_token: token)
 
     assert_response :success
   end
 
   test "patch update with matching passwords redirects to login" do
+    token = @user.send_reset_password_instructions
+
     assert_changes -> { @user.reload.password_digest } do
-      patch password_path(@user.password_reset_token), params: {
-        password: "new-password",
-        password_confirmation: "new-password"
+      patch user_password_path, params: {
+        user: {
+          reset_password_token: token,
+          password: "new-password",
+          password_confirmation: "new-password"
+        }
       }
     end
 
-    assert_redirected_to new_session_path
+    assert_redirected_to new_user_session_path
   end
 
   test "patch update with mismatched passwords returns unprocessable entity" do
-    token = @user.password_reset_token
+    token = @user.send_reset_password_instructions
 
     assert_no_changes -> { @user.reload.password_digest } do
-      patch password_path(token), params: {
-        password: "new-password",
-        password_confirmation: "different-password"
+      patch user_password_path, params: {
+        user: {
+          reset_password_token: token,
+          password: "new-password",
+          password_confirmation: "different-password"
+        }
       }
     end
 
