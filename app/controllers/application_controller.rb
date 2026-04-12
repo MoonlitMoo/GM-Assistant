243d316eda4adb567f0725e0d9fb11201661e814
Ben Colquhoun <benjamincahoonz@gmail.com>
2026-04-12 17:36:26 +0900

    fix: Default dev user + cleanup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
-  before_action :set_current_user
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
@@ -10,10 +9,6 @@ class ApplicationController < ActionController::Base
 
   def after_sign_out_path_for(_resource_or_scope)
     new_user_session_path
-  end
-
-  def set_current_user
-    Current.user = current_user
   end
 
   def breadcrumbs
